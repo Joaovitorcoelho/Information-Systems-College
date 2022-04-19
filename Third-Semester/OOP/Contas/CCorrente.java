e94811ee2fd65184a3696d2d308cb7422e8e8842
Gianluca Starke <48135682+XerlockHomeless@users.noreply.github.com>
2022-04-19 10:45:23 -0300

    Create CCorrente.java

---
+++ Third-Semester/OOP/Contas/CCorrente.java
@@ -0,0 +1,12 @@
+class CCorrente {
+
+    private int numero;
+    private float saldo;
+    private Clientes cliente;
+    
+    public CCorrente (int num, float sal, Clientes cli){
+ 
+        this.numero = num;
+        this.saldo = sal;
+        this.cliente = cli;
+    }
