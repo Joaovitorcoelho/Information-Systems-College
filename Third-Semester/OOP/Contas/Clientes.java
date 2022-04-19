01af4bf0e910094a3ed7d56414adcbd060308506
Gianluca Starke <48135682+XerlockHomeless@users.noreply.github.com>
2022-04-19 10:43:38 -0300

    Create Clientes.java

---
+++ Third-Semester/OOP/Contas/Clientes.java
@@ -0,0 +1,21 @@
+public class Clientes {
+
+    private String nome;
+    private String cpf;
+
+    public Clientes(String nome, String cpf){
+    
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+    
+    public String getNome(){
+    
+        return this.nome;
+    }
+    
+    public String getCpf(){
+    
+        return this.cpf;
+    }
+}
