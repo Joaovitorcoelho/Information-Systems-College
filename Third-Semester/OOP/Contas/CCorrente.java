54555af0ae91a101f5140c41fef951e4b18fad25
Gianluca Starke <48135682+XerlockHomeless@users.noreply.github.com>
2022-04-19 15:16:23 -0300

    Update CCorrente.java

--- Third-Semester/OOP/Contas/CCorrente.java
+++ Third-Semester/OOP/Contas/CCorrente.java
@@ -2,11 +2,38 @@ class CCorrente {
 
     private int numero;
     private float saldo;
-    private Clientes cliente;
+    private String cliente;
     
-    public CCorrente (int num, float sal, Clientes cli){
+    public CCorrente (int num, float sal, String cli){
  
         this.numero = num;
         this.saldo = sal;
         this.cliente = cli;
+    }
+
+    public void creditar(double valor){
+    
+        this.saldo = this.saldo + valor;
+    }
+
+    public void debitar(double valor){
+    
+        if (valor <= this.saldo) {
+    
+            this.saldo = this.saldo - valor;
+        }
+        else {
+    
+            System.out.println("Saldo Insuficiente");
+        }
+    }
+
+    public double getSaldo(){
+        
+        return this.saldo;
+    }
+    
+    public void setSaldo(double saldo){
+    
+        this.saldo = saldo;
     }
